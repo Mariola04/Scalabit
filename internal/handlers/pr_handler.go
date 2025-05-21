523ffafa68d37084283b7e4d511b328d32653862
Mariola04 <mariominhava@gmail.com>
2025-05-21 14:59:00 +0100

    added .yml file, pipeline implemented(?)

--- internal/handlers/pr_handler.go
+++ internal/handlers/pr_handler.go
@@ -8,6 +8,21 @@ import (
 	"github.com/Mariola04/Scalabit/internal/services"
 )
 
+
+/*
+In Gin, every HTTP request is handled by a function called a handler, and Gin gives that function a *gin.Context object as a parameter.
+
+This object (*gin.Context, usually called c) gives you full access to:
+
+    The request (method, URL, params, headers, body, etc...)
+
+    The response (status code, JSON response, etc...)
+
+    Middleware and other request-scoped values
+
+    *gin.Context is like the "interface" between my code and the incoming HTTP request/response.
+*/
+
 func ListPullRequests(c *gin.Context) {
 	owner := c.Param("owner")
 	repo := c.Param("repo")
